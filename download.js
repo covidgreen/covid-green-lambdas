d36f3a467e4b9c0ccd0ece24f87a32944b1a04f4
Jack Murdoch <35329295+jackmurdoch@users.noreply.github.com>
2020-07-31 10:53:32 +0100

    Add interop integration lambdas, remove unused lambdas (#3)

---
+++ download.js
@@ -0,0 +1,113 @@
+const fetch = require('node-fetch')
+const querystring = require('querystring')
+const SQL = require('@nearform/sql')
+const { getDatabase, getInteropConfig, runIfDev } = require('./utils')
+
+async function getFirstBatchTag(client) {
+  const query = SQL`
+    SELECT batch_tag AS "batchTag"
+    FROM download_batches
+    ORDER BY created_at DESC
+    LIMIT 1
+  `
+
+  const { rowCount, rows } = await client.query(query)
+
+  if (rowCount === 0) {
+    return ''
+  }
+
+  const [{ batchTag }] = rows
+
+  return batchTag
+}
+
+async function insertBatch(client, batchTag) {
+  const query = SQL`
+    INSERT INTO download_batches (batch_tag)
+    VALUES (${batchTag})
+  `
+
+  await client.query(query)
+}
+
+async function insertExposures(client, exposures) {
+  const query = SQL`INSERT INTO exposures (key_data, rolling_period, rolling_start_number, transmission_risk_level, regions) VALUES `
+
+  for (const [
+    index,
+    {
+      keyData,
+      rollingPeriod,
+      rollingStartNumber,
+      transmissionRiskLevel,
+      regions
+    }
+  ] of exposures.entries()) {
+    query.append(
+      SQL`(
+        ${keyData},
+        ${rollingPeriod},
+        ${rollingStartNumber},
+        ${transmissionRiskLevel},
+        ${regions}
+      )`
+    )
+
+    if (index < exposures.length - 1) {
+      query.append(SQL`, `)
+    }
+  }
+
+  query.append(
+    SQL` ON CONFLICT ON CONSTRAINT exposures_key_data_unique DO NOTHING`
+  )
+
+  await client.query(query)
+}
+
+exports.handler = async function () {
+  const { maxAge, token, url } = await getInteropConfig()
+  const client = await getDatabase()
+  const date = new Date()
+
+  date.setDate(date.getDate() - maxAge)
+
+  let more = true
+  let batchTag = await getFirstBatchTag(client)
+
+  do {
+    const query = querystring.stringify({ batchTag })
+    const downloadUrl = `${url}/download/${date.toISOString().substr(0, 10)}?${query}`
+
+    const response = await fetch(downloadUrl, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      }
+    })
+
+    if (response.status === 200) {
+      const data = await response.json()
+
+      batchTag = data.batchTag
+
+      if (data.exposures.length > 0) {
+        await insertExposures(client, data.exposures)
+      }
+
+      await insertBatch(client, batchTag)
+
+      console.log(`added ${data.exposures.length} exposures from batch ${batchTag}`)
+    } else if (response.status === 204) {
+      more = false
+
+      console.log('no more batches to download')
+    } else {
+      throw new Exception('Request failed')
+    }
+  } while (more)
+}
+
+runIfDev(exports.handler)
---
+++ upload.js
@@ -0,0 +1,82 @@
+const fetch = require('node-fetch')
+const SQL = require('@nearform/sql')
+const { JWK, JWS } = require('node-jose')
+const { getDatabase, getInteropConfig, runIfDev } = require('./utils')
+
+async function createBatch(client, count, lastExposureId) {
+  const query = SQL`
+    INSERT INTO upload_batches (exposure_count, last_exposure_id)
+    VALUES (${count}, ${lastExposureId})
+    RETURNING id
+  `
+
+  const { rows } = await client.query(query)
+  const [{ id }] = rows
+
+  return id
+}
+
+async function getFirstExposureId(client) {
+  const query = SQL`
+    SELECT COALESCE(MAX(last_exposure_id), 0) AS "firstExposureId"
+    FROM upload_batches
+  `
+
+  const { rows } = await client.query(query)
+  const [{ firstExposureId }] = rows
+
+  return firstExposureId
+}
+
+async function getExposures(client, since) {
+  const query = SQL`
+    SELECT id, created_at, key_data, rolling_period, rolling_start_number, transmission_risk_level, regions
+    FROM exposures
+    WHERE id > ${since}
+    ORDER BY id ASC
+  `
+
+  const { rows } = await client.query(query)
+
+  return rows
+}
+
+exports.handler = async function () {
+  const { privateKey, token, url } = await getInteropConfig()
+  const client = await getDatabase()
+  const firstExposureId = await getFirstExposureId(client)
+  const exposures = await getExposures(client, firstExposureId)
+
+  if (exposures.length > 0) {
+    const lastExposureId = exposures[exposures.length - 1].id
+    const batchTag = await createBatch(client, exposures.length, lastExposureId)
+    const key = await JWK.asKey(privateKey, 'pem')
+    const sign = JWS.createSign({ format: 'compact' }, key)
+
+    const payload = exposures.map(({ key_data, rolling_start_number, transmission_risk_level, rolling_period, regions }) => ({
+      keyData: key_data,
+      rollingStartNumber: rolling_start_number,
+      transmissionRiskLevel: transmission_risk_level,
+      rollingPeriod: rolling_period,
+      regions: regions
+    }))
+
+    await fetch(`${url}/upload`, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        batchTag,
+        payload: await sign.update(JSON.stringify(payload), 'utf8').final()
+      })
+    })
+
+    console.log(`uploaded ${exposures.length} to batch ${batchTag}`)
+  } else {
+    console.log('no exposures to upload')
+  }
+}
+
+runIfDev(exports.handler)
