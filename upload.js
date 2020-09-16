815af5a56909fe3c9f0bf7d8e50f2218d2746cea
Jack Murdoch <jack.murdoch@nearform.com>
2020-09-16 15:24:33 +0100

    EFGS compatability

--- upload.js
+++ upload.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
+const jsrsasign = require('jsrsasign')
 const SQL = require('@nearform/sql')
-const { JWK, JWS } = require('node-jose')
 const {
   getDatabase,
   getInteropConfig,
@@ -35,9 +35,9 @@ async function getFirstExposureId(client) {
 
 async function getExposures(client, since) {
   const query = SQL`
-    SELECT id, created_at, key_data, rolling_period, rolling_start_number, transmission_risk_level, regions
+    SELECT id, created_at, key_data, rolling_period, rolling_start_number, transmission_risk_level, regions, days_since_onset
     FROM exposures
-    WHERE id > ${since}
+    WHERE id > ${since} AND origin IS NULL
     ORDER BY id ASC
   `
 
@@ -47,7 +47,7 @@ async function getExposures(client, since) {
 }
 
 exports.handler = async function() {
-  const { privateKey, token, url } = await getInteropConfig()
+  const { certificate, origin, privateKey, token, url } = await getInteropConfig()
   const client = await getDatabase()
   const firstExposureId = await getFirstExposureId(client)
   const exposures = await getExposures(client, firstExposureId)
@@ -64,35 +64,89 @@ exports.handler = async function() {
         exposures.length,
         lastExposureId
       )
-      const key = await JWK.asKey(privateKey, 'pem')
-      const sign = JWS.createSign({ format: 'compact' }, key)
 
-      const payload = exposures.map(
-        ({
-          key_data: keyData,
-          rolling_start_number: rollingStartNumber,
-          transmission_risk_level: transmissionRiskLevel,
-          rolling_period: rollingPeriod,
-          regions
-        }) => ({
-          keyData: keyData,
-          rollingStartNumber: rollingStartNumber,
-          transmissionRiskLevel: transmissionRiskLevel,
-          rollingPeriod: rollingPeriod,
-          regions: regions
-        })
+      const reportTypes = {
+        CONFIRMED_TEST: 1,
+        CONFIRMED_CLINICAL_DIAGNOSIS: 2,
+        SELF_REPORT: 3,
+        RECURSIVE: 4,
+        REVOKED: 5
+      }
+
+      const keys = exposures.map(({ key_data, rolling_start_number, transmission_risk_level, rolling_period, regions, days_since_onset }) => ({
+        keyData: key_data,
+        rollingStartIntervalNumber: rolling_start_number,
+        transmissionRiskLevel: transmission_risk_level,
+        rollingPeriod: rolling_period,
+        visitedCountries: regions,
+        reportType: 'CONFIRMED_CLINICAL_DIAGNOSIS',
+        days_since_onset_of_symptoms: days_since_onset,
+        origin
+      }))
+
+      const data = Buffer.concat(
+        keys
+          .sort((a, b) => {
+            if (a.keyData < b.keyData) {
+              return -1
+            }
+      
+            if (a.keyData > b.keyData) {
+              return 1
+            }
+      
+            return 0
+          })
+          .map(({ keyData, rollingStartIntervalNumber, rollingPeriod, transmissionRiskLevel, visitedCountries, origin, reportType, days_since_onset_of_symptoms }) => {
+            const rollingStartIntervalNumberBuffer = Buffer.alloc(4)
+            const rollingPeriodBuffer = Buffer.alloc(4)
+            const transmissionRiskLevelBuffer = Buffer.alloc(4)
+            const reportTypeBuffer = Buffer.alloc(4)
+            const daysSinceOnsetOfSymptomsBuffer = Buffer.alloc(4)
+      
+            rollingStartIntervalNumberBuffer.writeUInt32BE(rollingStartIntervalNumber)
+            rollingPeriodBuffer.writeUInt32BE(rollingPeriod)
+            transmissionRiskLevelBuffer.writeInt32BE(transmissionRiskLevel)
+            reportTypeBuffer.writeInt32BE(reportTypes[reportType] || 0)
+            daysSinceOnsetOfSymptomsBuffer.writeUInt32BE(days_since_onset_of_symptoms)
+      
+            return Buffer.concat([
+              Buffer.from(Buffer.from(keyData, 'base64').toString('utf-8')),
+              rollingStartIntervalNumberBuffer,
+              rollingPeriodBuffer,
+              transmissionRiskLevelBuffer,
+              ...visitedCountries.map(country => Buffer.from(country)),
+              Buffer.from(origin),
+              reportTypeBuffer,
+              daysSinceOnsetOfSymptomsBuffer
+            ])
+          })
       )
+      
+      const signed = jsrsasign.KJUR.asn1.cms.CMSUtil.newSignedData({
+        content: { hex: data.toString('hex') },
+        certs: [certificate],
+        detached: false,
+        signerInfos: [{
+          hashAlg: 'sha256',
+          sAttr: {
+            SigningTime: {}
+          },
+          signerCert: certificate,
+          sigAlg: 'SHA1withRSA',
+          signerPrvKey: privateKey
+        }]
+      })
 
       const result = await fetch(`${url}/upload`, {
         method: 'POST',
         headers: {
           Authorization: `Bearer ${token}`,
+          batchSignature: Buffer.from(signed.getContentInfoEncodedHex(), 'hex').toString('base64'),
+          batchTag,
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify({
-          batchTag,
-          payload: await sign.update(JSON.stringify(payload), 'utf8').final()
-        })
+        body: JSON.stringify({ keys })
       })
 
       if (!result.ok) {
